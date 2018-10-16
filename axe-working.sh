2b3690e7c6d9ff959d29a46e93475b885f8f8dc8
LloydVersion1 <lloyd.obrien@version1.com>
2018-10-16 16:23:10 +0000

    adding new commit

---
+++ axe-working.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+#A Bash script to install the AWS AXE tool created by @kxseven. This script needs to run as "$ sudo su" && "./axe-install.sh".
+
+apt-get install -y \
+libkrb5-dev \
+python-gssapi \
+python-kerberos \
+python-requests-kerberos \
+python-simplejson \
+bash-completion \
+git \
+jq \
+graphviz \
+build-essential \
+python-pip \
+libssl-dev \
+python-dev \
+
+sudo apt install virtualenv
+
+git clone https://bitbucket.org/kxseven/axe.git
+
+cd axe
+AXE_ROOT=$(pwd)
+export AXE_ROOT
+cd $AXE_ROOT
+virtualenv local/python
+source $AXE_ROOT/etc/axerc
+pip install -r requirements/requirements.python2 -I
+
+cat >> ~/.bashrc <<-EOF
+# AXE Environment
+AXE_ROOT=$AXE_ROOT
+PATH=\$PATH:\$AXE_ROOT/bin
+export AXE_ROOT PATH
+# Useful Aliases
+alias goaxe='cd \$AXE_ROOT; . etc/axerc'
+
+EOF
+
